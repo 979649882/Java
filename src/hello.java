996662c239fbc99e7ce2d72e2f5cedc0f77b8e8b
YifeiLin0226 <bobbylin0226@126.com>
2020-02-12 17:30:38 -0700

    add

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,17 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		
+		for(int i=5;i>=0;i--) {
+		int j=i;
+			while(j>=0) {
+				System.out.print("*");
+				j--;
+			}
+				
+			
+			System.out.println();
+		}
 	}
 
 }
